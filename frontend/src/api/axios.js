d5199e33965e09606fb71060826aca77926bc143
Aman98t <amankumar9877745471@gmail.com>
2026-07-24 12:30:02 +0530

    Updated API base URL to live Render server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://notenest-backend-n4i4.onrender.com/api',
 });
 
 // Request Interceptor: Automatically attach token if available
